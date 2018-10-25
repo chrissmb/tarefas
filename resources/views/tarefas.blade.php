dbe4ab752cba0f747e589810319eeaa2b1580c67
Chris <chrissantista@gmail.com>
2018-10-25 20:46:45 -0300

    .

--- resources/views/tarefas.blade.php
+++ resources/views/tarefas.blade.php
@@ -3,9 +3,34 @@
 @section('title', 'Tarefas')
 
 @section('content')
-    <form class="form-inline" action="teste">
-        <input type="text" class="form-control" placeholder="Tarefa">
+    @include('common.errors')
+    <form class="form-inline" action="{{ url('tarefas') }}" method="POST">
+        @csrf
+        <input type="text" class="form-control" name="descricao" placeholder="Tarefa">
         <button type="submit" class="btn btn-primary">Adicionar</button>
     </form>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Descrição</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($tarefas as $tarefa)
+                <tr>
+                    <td>{{ $tarefa->descricao }}</td>
+                    <td>
+                        <form action="{{ url('tarefas/'.$tarefa->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input class="btn btn-danger" type="submit" value="delete">
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Use App\Tarefa;
+Use Illuminate\Http\Request;
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -21,12 +23,44 @@ Route::get('/', function () {
 
 Route::get('/teste', function () {
     return "Olha eu!";
-});
+})->middleware('auth');
+
+Route::get('/login', function () {
+    return "Página de login";
+})->name('login');
 
 Route::get('user/{id}', function ($id) {
     return 'User '.$id;
 });
 
-Route::get('tarefas/', function () {
-    return view('tarefas');
+Route::get('/tarefas', function () {
+    $tarefas = Tarefa::all();
+    return view('tarefas', ['tarefas' => $tarefas]);
+});
+
+Route::get('/tarefas-json', function () {
+    return Tarefa::all();
+});
+
+Route::post('/tarefas', function (Request $req) {
+    $validator = Validator::make($req->all(), [
+        'descricao' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $tarefa = new Tarefa;
+    $tarefa->descricao = $req->descricao;
+    $tarefa->save();
+
+    return redirect('/tarefas');
+});
+
+Route::delete('tarefas/{tarefa}', function (Tarefa $tarefa) {
+    $tarefa->delete();
+    return redirect('/tarefas');
 });
